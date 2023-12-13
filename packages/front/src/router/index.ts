65b0d17bfb3b887b650b0c9132a888d8ee2df03c
DeschuytterBjarni <bjarni.deschuytter@student.howest.be>
2023-12-13 16:10:19 +0100

    fix: index.ts

--- packages/front/src/router/index.ts
+++ packages/front/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
-import DoctorDashboard from '../views/DoctorDashboard.vue'
+import DoctorDashboard from '../views/doctorDashboard.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
