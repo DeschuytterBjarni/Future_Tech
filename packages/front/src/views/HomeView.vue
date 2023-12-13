3c80faefa38cff1aeb8a6dc6a9d87a48eb49190a
Casper Van Damme <casper.van.damme@student.howest.be>
2023-12-13 11:03:42 +0100

    fix: tailwind working

--- packages/front/src/views/HomeView.vue
+++ packages/front/src/views/HomeView.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="flex h-screen items-center justify-center">
       <div class="text-center">
-        <h1 class="text-4xl font-bold mb-6">Welcome to the revalidation aid</h1>
+        <h1 class="text-6xl font-bold mb-6">Welcome to the revalidation aid</h1>
         <router-link to="/patient">
           <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-2">
             Patient
