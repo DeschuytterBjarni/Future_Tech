0fc8d7488c5fb1f5c3fdc2e36c3a2233b3b73b1e
alessia vinnik <alessia.vinnik@student.howest.be>
2023-12-13 11:06:22 +0100

    Auto stash before merge of "feature/frontend-doctor-dashboard" and "origin/feature/login-page"

--- packages/front/src/router/index.ts
+++ packages/front/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +8,10 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     }
+    // {
+    //   path: '/:pathMatch(.*)*',
+    //   component: () => import('../views/NotFound.vue'),
+    // },
   ]
 })
 
---
+++ packages/front/src/views/Dashboard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="rounded-lg border bg-card text-card-foreground shadow-sm">
+      <div class="flex flex-col space-y-1.5 p-6">
+        <div class="flex items-center">
+          <span class="relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full"></span>
+          <div class="ml-4">
+            <h3 class="text-2xl font-semibold leading-none tracking-tight">{{ patient.name }}</h3>
+            <a class="text-sm text-blue-600" rel="ugc">View Profile</a>
+          </div>
+        </div>
+      </div>
+      <div class="p-6">
+        <div class="grid gap-4">
+          <div class="flex items-center">
+            <!-- <person-standing/> -->
+            <span>{{ patient.activity }}</span>
+          </div>
+          <div class="flex items-center">
+            <span>{{ patient.duration }}</span>
+          </div>
+          <div class="flex items-center">
+            <span>{{ patient.frequency }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { X } from 'lucide-vue-next';
+  export default {
+    components: {
+        X
+    },
+    props: ['patient'],
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>
+  
