3853bc93c0f30517c235e4c73610b67a2a00748b
Casper Van Damme <casper.van.damme@student.howest.be>
2023-12-13 10:35:31 +0100

    feat: homeview

--- packages/front/src/views/HomeView.vue
+++ packages/front/src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="flex h-screen items-center justify-center">
+      <div class="text-center">
+        <h1 class="text-4xl font-bold mb-6">Welcome to the revalidation aid</h1>
+        <router-link to="/patient">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mx-2">
+            Patient
+          </button>
+        </router-link>
+        <router-link to="/doctor">
+          <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full mx-2">
+            Doctor
+          </button>
+        </router-link>
+      </div>
+    </div>
 </template>
