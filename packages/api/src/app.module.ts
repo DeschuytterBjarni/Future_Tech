f8672b3cc8f93cfb7483fb5c7e5614833ca7cf4c
kelvindesaeger <kelvin.de.saeger@student.howest.be>
2023-12-13 12:02:41 +0100

    add first routes users

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost:27031/futureTech',
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: true, // Careful with this in production
+      useNewUrlParser: true,
+      useUnifiedTopology: true, // Disable deprecated warnings
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
