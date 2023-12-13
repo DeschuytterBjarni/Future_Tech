b4db5bc1932a12f53baeb7c59bc8bfcf465543f3
alessia vinnik <alessia.vinnik@student.howest.be>
2023-12-13 15:27:33 +0100

    fix: logout buttons bij doctor

--- packages/front/src/views/PatientDetail.vue
+++ packages/front/src/views/PatientDetail.vue
@@ -16,6 +16,15 @@
         <span class="flex items-center text-lg font-semibold md:text-base"
           >Profile</span
         >
+        <div class="flex-shrink-0 px-4 py-2">
+          <RouterLink to="/">
+            <button
+              class="inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2"
+            >
+              Logout
+            </button>
+          </RouterLink>
+        </div>
       </nav>
     </header>
     <main
