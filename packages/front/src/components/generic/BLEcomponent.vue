9f7102f08410332327d4afd41106ee48b2573ebe
DeschuytterBjarni <bjarni.deschuytter@student.howest.be>
2023-12-13 10:43:33 +0100

    feat: BLE test component

---
+++ packages/front/src/components/generic/BLEcomponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="requestDevice()">
+        Request Bluetooth Device
+    </button>
+</template>
+  
+<script>
+import { useBluetooth } from '@vueuse/core'
+
+const {
+    isSupported,
+    isConnected,
+    device,
+    requestDevice,
+    server,
+} = useBluetooth({
+    acceptAllDevices: true,
+})
+</script>
+  
